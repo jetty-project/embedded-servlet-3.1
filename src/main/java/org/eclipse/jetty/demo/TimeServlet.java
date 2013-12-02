c1d31793776c7c812a3141283cd6a73d6331bf40
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-12-02 12:36:00 -0700

    Initial codebase

---
+++ src/main/java/org/eclipse/jetty/demo/TimeServlet.java
@@ -0,0 +1,22 @@
+package org.eclipse.jetty.demo;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns =
+{ "/time" })
+public class TimeServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/plain");
+        response.getWriter().write(new Date().toString());
+    }
+}
---
+++ src/test/java/org/eclipse/jetty/demo/EmbedMe.java
@@ -0,0 +1,47 @@
+package org.eclipse.jetty.demo;
+
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.TagLibConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+
+public class EmbedMe
+{
+    public static void main(String[] args) throws Exception
+    {
+        int port = 8080;
+        Server server = new Server(port);
+
+        String wardir = "target/embedded-servlet-3.1-1-SNAPSHOT";
+
+        WebAppContext context = new WebAppContext();
+        context.setResourceBase(wardir);
+        context.setDescriptor(wardir + "WEB-INF/web.xml");
+        context.setConfigurations(new Configuration[]
+        { 
+            new AnnotationConfiguration(), 
+            new WebInfConfiguration(), 
+            new WebXmlConfiguration(), 
+            new MetaInfConfiguration(), 
+            new FragmentConfiguration(),
+            new EnvConfiguration(), 
+            new PlusConfiguration(), 
+            new JettyWebXmlConfiguration() 
+        });
+
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
+        server.setHandler(context);
+        server.start();
+        server.dump(System.err);
+        server.join();
+    }
+}
